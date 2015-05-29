58c9f0766831da0f1309d5fb3a3c2d45328d8142
boggogo <koemdjiev@gmail.com>
2015-05-30 00:27:28 +0100

    appCompact instead of ActionBar activity

--- app/src/main/java/koemdzhiev/com/materialdesigntabs/MainActivity.java
+++ app/src/main/java/koemdzhiev/com/materialdesigntabs/MainActivity.java
@@ -2,13 +2,13 @@ package koemdzhiev.com.materialdesigntabs;
 
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     // Declaring Your View and Variables
 
@@ -16,8 +16,8 @@ public class MainActivity extends ActionBarActivity {
     ViewPager pager;
     ViewPagerAdapter adapter;
     SlidingTabLayout tabs;
-    CharSequence Titles[]={"Home","Events"};
-    int Numboftabs =2;
+    CharSequence Titles[]={"Home","Events","Media"};
+    int Numboftabs =3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
